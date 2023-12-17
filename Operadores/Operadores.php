c58c56ca55a64ddeae4d507ed844e89769b650db
Jeremías <jeremiias122002@gmail.com>
2023-12-17 12:32:55 +0100

    cuarta carpeta de repaso

---
+++ Operadores/Operadores.php
@@ -0,0 +1,65 @@
+<?php
+
+    /*
+        addition | +
+        subtraction | -
+        multiplication | *
+        division | /
+        modulo | %
+        exponentiation | **
+
+        equal to = "
+
+        ARRAY OPERATORS
+        - Union +
+        - Equality == 
+        - Identity ===
+        - Inequality !=
+        - Inequality <>
+        Non-identity !==
+
+        BITWISE OPERATOR
+        & = and
+        ^ = xor
+        | = not
+        << = shift left
+        >> = shift right
+
+        COMPARISON OPERATOR
+        equal ==
+        identical ===
+        not equal !=
+        not identical !==
+        not equal <>
+        less than <
+        less than or equal <=
+        greater than >
+        greater than or equal >=
+        spaceship operator <=>
+
+        EXECUTION OPERATOR
+        backticks = ``
+
+        ERROR CONTROL OPERATOR
+        @ error control operator
+
+        PRE & POST INCREMENT
+        ++$a $a++
+
+        PRE & POST DECREMENT
+        --$a $a--
+
+        LOGICAL OPERATOR
+        && and
+        || or
+        ! Not
+        and And
+        xor xor
+        or Or
+
+        STRING OPERATOR
+        . concatenation oper
+        .= concatenating assignment oper
+    */
+
+?>
